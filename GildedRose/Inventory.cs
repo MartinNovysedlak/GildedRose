b01377992a41dad5ee53d84319f18527a2cbc47a
Martin Novysedlák <martinnovysedlak48@gmail.com>
2019-12-12 09:09:24 +0100

    inital

--- GildedRose/Inventory.cs
+++ GildedRose/Inventory.cs
@@ -22,8 +22,57 @@ namespace GildedRose
         /// </summary>
         public void UpdateQuality()
         {
-            // TODO ...
-            // Hint: Iterate through this.items and check Name property to access specific item
+            foreach(var item in items)
+            {
+                if (item.Name == "Sulfuras")
+                {
+                    continue;
+                }
+                    item.SellIn--;
+                if(item.Name == "Aged Brie")
+                {
+                    item.Quality++;
+                    if(item.SellIn <= 0)
+                    {
+                        item.Quality++;
+                    }
+                }
+
+                if(item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    item.Quality++;
+                    if(item.SellIn <= 0)
+                    {
+                        item.Quality = 0;
+                    }
+                    if (item.SellIn <= 5)
+                    {
+                        item.Quality += 2;
+                    }
+                    if (item.SellIn <= 10)
+                    {
+                        item.Quality += 3;
+                    }
+                }
+                if (item.Name == "Conjured Mana Cake")
+                {
+                    item.Quality -= 2;
+
+                    if(item.SellIn <= 2)
+                    {
+                        item.Quality = 0;
+                    }
+                }
+
+                if (item.Quality <= 0) item.Quality = 0;
+                if (item.Quality >= 50) item.Quality = 50;
+                if (item.Name == "Sulfuras, Hand of Ragnaros")
+                {
+                    item.Quality = 80;
+                    item.SellIn = 0;
+                }
+
+            }
         }
     }
 }
